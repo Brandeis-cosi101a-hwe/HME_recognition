c837407850fa7a93adaa3523339fe8200fd034f2
LarryJiang134 <Larry.hangyan@gmail.com>
2017-05-06 07:22:19 -0400

    Add files via upload

--- DataPreparation/data_train_raw.py
+++ DataPreparation/data_train_raw.py
@@ -1,13 +1,21 @@
+import numpy
 import numpy as np
 import cv2
 import os
 import glob
 import ntpath
+import skimage
+from PIL import Image
+from skimage.exposure import exposure
+import skimage.transform
+import shutil
 
 # Overall: multiprocessing is not used, but mem is pre-allocated for better performance
 
 
 # All available labels generated from pic names (using set)
+
+
 categories = ['c', 's', 'y', 'pi', '3', '0', '6', '1', 'mul', 'p', 'cos', '2',
               'n', 'A', '-', 'pm', 'a', '+', '(', 'sqrt', 'sin', 'x', 'div', 'dots', 'tan',
               'bar', 'b', 'o', 't', 'h', 'delta', 'f', 'm', ')', 'd', '4', 'i', 'k', '=', 'frac']
@@ -19,7 +27,17 @@ def padding_square(img):
     :rtype: ndarray
     """
     # TODO: add padding to right/bottom side of img and make it a square
-
+    img = exposure.adjust_gamma(img, 0.15)
+    (vertical_pixel, horizontal_pixel) = img.shape
+    if vertical_pixel > horizontal_pixel:
+        vertical_padding = int(round(vertical_pixel * 0.15))
+        horizontal_padding = int(round((vertical_pixel * 1.3 - horizontal_pixel) / 2))
+        padding = ((vertical_padding, vertical_padding), (horizontal_padding, horizontal_padding))
+    else:
+        horizontal_padding = int(round(horizontal_pixel * 0.15))
+        vertical_padding = int(round((horizontal_pixel * 1.3 - vertical_pixel) / 2))
+        padding = ((vertical_padding, vertical_padding), (horizontal_padding, horizontal_padding))
+    img = skimage.util.pad(img, padding, 'constant', constant_values=0)
     return img
 
 
@@ -29,7 +47,8 @@ def padding_32(img):
     :rtype: ndarray
     """
     # TODO: add padding to 32*32
-
+    padding = ((2, 2), (2, 2))
+    img = skimage.util.pad(img, padding, 'constant', constant_values=0)
     return img
 
 
@@ -44,15 +63,18 @@ pic_nums = len(pics)
 
 # init x, y and pre-alloc mem
 x = np.empty((pic_nums * 32 * 32))
-y = np.empty((pic_nums))
+y = np.empty((pic_nums), dtype=str)
 
 pxl = 0  # pointer to next slot to insert img32 into x (pointer_x_left)
-py = 0 #pointer to append label
+py = 0 # pointer to append label
 
 for pic in pics:
-
     # get label from each pic name
     label = ntpath.basename(pic).split('_')[3]
+
+    #print(pic)
+    #print(label)
+    #print(py)
 
     # Readin pic
     img = cv2.imread(pic, 0) # 0 for grayscale 1 channel
@@ -69,7 +91,7 @@ for pic in pics:
     # flatten img to fill np array
     flatten = img.flatten()
 
-    #insert flatten to x
+    # insert flatten to x
     pxr = pxl + 32 * 32
     x[pxl:pxr] = flatten
     pxl = pxr
@@ -78,5 +100,5 @@ for pic in pics:
     y[py] = label
     py+=1
 
-#save to npz
+# save to npz
 np.savez_compressed('train32_raw.npz', x = x, y = y)
