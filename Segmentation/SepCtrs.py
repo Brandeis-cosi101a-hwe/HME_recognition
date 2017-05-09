24b3be48ca0e8e69b748a117500714e7edd98d69
LarryJiang134 <Larry.hangyan@gmail.com>
2017-05-08 20:35:29 -0400

    Add files via upload

--- Segmentation/SepCtrs.py
+++ Segmentation/SepCtrs.py
@@ -5,10 +5,30 @@ import cv2
 import numpy as np
 from skimage import measure
 
-im_gray = cv2.imread("test.png", 0)
+im_gray = cv2.imread("samp.png", 0)
 
 def getName(rect):
     return str(rect[0])+'_'+str(rect[0]+rect[2])+'_'+str(rect[1])+"_"+str(rect[1] + rect[3])+'.png'
+
+def targetNum(blobs_labels):
+    (h, w) = blobs_labels.shape
+    edge1 = blobs_labels[0]
+    edge2 = blobs_labels[h-1]
+    edge3 = blobs_labels[:,0]
+    edge4 = blobs_labels[:,w-1]
+    edges = []
+    edges.extend(edge1)
+    edges.extend(edge2)
+    edges.extend(edge3)
+    edges.extend(edge4)
+    edges = [x for x in edges if x != 0]
+    nums = list(set(edges))
+    for num in nums:
+        if (num in edge1) and (num in edge2) and (num in edge3) and (num in edge4):
+            print(num)
+            return num
+    return 0
+
 
 # Convert to grayscale and apply Gaussian filtering
 im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
@@ -22,7 +42,7 @@ for rect in rects:
     # create new img file
     im_temp = im2[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]]
     blobs_labels = measure.label(im_temp, connectivity=5, neighbors=8, background=0, return_num=False)
-    im_temp = (blobs_labels == 1) * 255
+    im_temp = (blobs_labels == targetNum(blobs_labels)) * 255
     cv2.imwrite(getName(rect), im_temp)
     # draw rectangle
     cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
