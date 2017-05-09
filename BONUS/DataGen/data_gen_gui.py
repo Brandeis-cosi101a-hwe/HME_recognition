b7265f3b678badd7b8d41736c8bebf856b2218d4
Jincheng Zhang <zhjch05@live.com>
2017-05-09 15:37:18 -0400

    sync

--- BONUS/DataGen/data_gen_gui.py
+++ BONUS/DataGen/data_gen_gui.py
@@ -4,16 +4,22 @@ import os
 import glob
 import matplotlib.pyplot as plt
 import pylab
+import ntpath
 
 
 def nothing(x):
     pass
 
 path = os.path.join(os.getcwd(), 'test', 'eqs')
+sym_path = os.path.join(os.getcwd(), 'test', 'syms')
 # all paths of pics
-pics = glob.glob(path + '/*.png')
+pics = glob.glob(path + '\\*.png')
 
 pic = pics[0]
 img = cv2.imread(pic, 0)
+syms = glob.glob(sym_path + '\\'+ ntpath.basename(pic).split('.')[0] + '_*.png')
+for sym in syms:
+    boudingRect = []
+    
 imgplot = plt.imshow(img)
 pylab.show()
